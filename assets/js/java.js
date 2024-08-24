47d28d63d2200a3f785454b3bbde42748126f563
BastianMartinez01 <basinzunza@gmail.com>
2024-08-23 22:00:18 -0400

    commit 2

--- assets/js/java.js
+++ assets/js/java.js
@@ -28,3 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
         bootstrapCarousel.cycle();
     });
 });
+
+(function() {
+    'use strict';
+  
+    var forms = document.querySelectorAll('.needs-validation');
+  
+    Array.prototype.slice.call(forms).forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+  
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+  
